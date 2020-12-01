d4e01430457838122a85a323fd0c97f548625bb1
zhang0135789 <zz634682577@163.com>
2020-12-01 11:25:20 +0800

    init demo hzwq

--- demo12_hzwq/contracts/Hzwq.sol
+++ demo12_hzwq/contracts/Hzwq.sol
@@ -3,6 +3,8 @@ pragma solidity >= 0.5.0 < 0.7.4;
 
 
 import "./provableAPI.sol";
+import "./StringUtils.sol";
+
 
 contract Hzwq is usingProvable{
   constructor() public {
@@ -10,13 +12,10 @@ contract Hzwq is usingProvable{
 
   }
 
-  string public ETHUSD;
 
   event LogNewOraclizeQuery(string description);
   event LogNewOraclizeQuery2(string description);
   event LogPriceUpdated(string price);
-
-
 
 
 
@@ -26,11 +25,55 @@ contract Hzwq is usingProvable{
     return 1==1;
   }
 
-  //唯一码
-  function onlyCode() public pure returns (string memory) {
+  uint256 MAX_random = 100;
+  string  public randomStr ;
 
-    return "";
+  mapping(uint256 => uint256) public randomMap;
+
+  //唯一码
+  function onlyCode(string memory json) public
+  returns (
+    bytes32 ,
+    string memory)
+  {
+    bytes32 hash = keccak256(bytes(json));
+
+
+//    if(random > MAX_random) {
+//      random = 0;
+//    }
+
+
+//    string memory result = hash + randomStr;
+    //    string memory randomStr = uint2str(random++);
+    //    string memory result = StringUtils.strConcat(hash , randomStr);
+    //    return result;
+    return (hash , randomCode());
   }
+
+  uint random = 0;
+
+  // 自增码
+  function randomCode() public payable returns(string memory) {
+
+    if(randomMap[0] == 0) {
+      randomMap[0] = 1;
+    }else {
+      randomMap[0] = randomMap[0] + 1;
+    }
+
+
+    randomStr = uint2str(randomMap[0]);
+    return randomStr;
+  }
+
+  function getRandomMap(uint _index) public view returns(uint){
+    return randomMap[_index];
+  }
+
+  //    string memory hash = StringUtils.bytes32ToString(keccak256(abi.encode(json)));
+
+
 
 
   //审批
@@ -39,8 +82,11 @@ contract Hzwq is usingProvable{
   }
 
 
+  string public result;
+  bytes32 public myid;
+
   //签章
-  function signature() public payable {
+  function signature(string memory _url) public payable {
 
     if (provable_getPrice("URL") >= address(this).balance) {
 
@@ -48,17 +94,19 @@ contract Hzwq is usingProvable{
       //            provable_query("URL", "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price");
       //            provable_query("URL", "json(http://192.168.15.177:10080/blockServer/cunzheng/data/query?hash=123)");
       //            provable_query("URL", "https://smlopenapi.esign.cn/v1/oauth2/access_token?appId=7438830459&secret=c1b5ae47815c51af51e0ffdda1cd0693&grantType=client_credentials");
-      provable_query("URL", "https://api.pro.coinbase.com/products/ETH-USD/ticker");
-
+      provable_query("URL", _url);
 
     }
 
   }
   function __callback( bytes32 _myid, string memory _result ) public {
     if (msg.sender != provable_cbAddress()) revert();
-    ETHUSD = _result;
+    result = _result;
+    myid = _myid;
+
     // Do something with viewsCount, like tipping the author if viewsCount > X?
   }
+
 
 
 
---
+++ demo12_hzwq/contracts/StringUtils.sol
@@ -0,0 +1,59 @@
+
+pragma solidity >= 0.5.1 < 0.7.0;
+
+library StringUtils {
+
+
+
+        /**
+            对两个字符串进行拼接
+        */
+    function strConcat(string memory _a, string memory _b) internal pure returns (string memory){
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        string memory ret = new string(_ba.length + _bb.length );
+        bytes memory bret = bytes(ret);
+        uint k = 0;
+        uint i;
+        for (i = 0; i < _ba.length; i++){
+            bret[k++] = _ba[i];
+        }
+        for (i = 0; i < _bb.length; i++) {
+            bret[k++] = _bb[i];
+        }
+        return string(ret);
+    }
+
+
+    // 如果是固定大小字节数组转string，那么就需要先将字节数组转动态字节数组，再转字符串。
+    // 但是，如果字符串不是占满32个字节。那么后面就会由0进行填充。所以我们需要将这些空字符去掉。
+    function bytes32ToString(bytes32 b32name) internal pure returns(string memory){
+
+        bytes memory bytesString = new bytes(32);
+
+        // 定义一个变量记录字节数量
+        uint charCount = 0;
+
+        uint32 i = 0;
+        // 统计共有多少个字节数
+        for( i = 0; i < 32; i++){
+
+            byte char = byte(bytes32(uint(b32name) * 2 ** (8 * i)));  // 将b32name左移i位,参考下面算法
+            // 获取到的始终是第0个字节。
+            // 但为什么*2
+
+            if(char != 0){
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+
+        // 初始化一动态数组，长度为charCount
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for(i = 0;i< charCount;i++){
+            bytesStringTrimmed[i] = bytesString[i];
+        }
+
+        return string(bytesStringTrimmed);
+    }
+}
